0ef80ceda98d18baf3dcb620a237af74698df48f
JagdishSingh01 <jatinpawar354@gmail.com>
2025-05-01 12:02:40 +0530

    todo app made using cubit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todolist_app_with_cubit/add_todo_page.dart';
+import 'package:todolist_app_with_cubit/cubit/todo_cubit.dart';
+import 'package:todolist_app_with_cubit/todo_list.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => TodoCubit(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        initialRoute: '/',
+        routes: {
+          '/': (_) => const TodoList(),
+          '/add-todo': (_) => const AddTodoPage(),
+        },
+      ),
+    );
+  }
+}
